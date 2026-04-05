0e140cece42f27e87ce6b8cac7aa0448be939a1d
Gabriel <g.abreu@aluno.ufabc.edu.br>
2026-04-05 00:38:28 -0300

    chore: initial project structure with vite and react-ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App(){
+  return (
+    <main>
+      <h1>Aresta UFABC</h1>
+      <p>O grafo de recomendações começa aqui.</p>
+      {/* Aqui vai entrar o seu componente de Grafo depois */}
+    </main>
+  )
+}
+
+export default App
